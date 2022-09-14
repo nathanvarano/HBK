e855156b874b7473097187ec1247933d92157ba8
Nathan Varano <nathanvarano@gmail.com>
2022-09-14 17:50:57 +1000

    Initial commit
    - Includes Laravel
    - Includes Vue

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue"
+import App from './vue/app.vue'
+
+createApp(App).mount("#app");
---
+++ resources/js/vue/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>
+        Initial commit
+    </h1>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
